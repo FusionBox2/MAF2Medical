8fed46786e9b34e7ff4871d2f11728abd69d9541
besoft <besoft@kiv.zcu.cz>
2019-07-31 15:57:22 +0200

    Added explicit specification of the platform x86 because CMake 3.15 switched the deault to x64.

--- Libraries/Configure.cmake
+++ Libraries/Configure.cmake
@@ -13,7 +13,7 @@ INCLUDE (${MedFL_SOURCE_DIR}/modules/MedFLConfigMacro.cmake)
 SET (DEBUG FALSE)
 
 # options for configuring MedFL libraries
-#OPTION(MED_USE_Offis "Find and Link the Offis library." ON)
+OPTION(MED_USE_Offis "Find and Link the Offis library." ON)
 #IF (${CMAKE_GENERATOR} STREQUAL "Visual Studio 9 2008" OR ${CMAKE_GENERATOR} STREQUAL "Visual Studio 10")
   OPTION(MED_USE_BTK "Find and Link the BTK library." ON)
 #ELSE (${CMAKE_GENERATOR} STREQUAL "Visual Studio 9 2008" OR ${CMAKE_GENERATOR} STREQUAL "Visual Studio 10")
